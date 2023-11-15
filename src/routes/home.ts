5baf239c809e274d4efa95de0e3fca6bbcdc9ee6
neo-navnn <neo.navnn@gmail.com>
2023-11-16 00:36:06 +0530

    feat: implement suggested & trending routes

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,6 +3,7 @@ import { Action } from '@db/models/action.model'
 import { Charity } from '@db/models/charity.model'
 import { News } from '@db/models/news.model'
 import { Event } from '@db/models/event.model'
+import { SDG } from '@db/models'
 
 import { ActionResponseType } from '@validations/action.validation'
 // import { zodSDGRelatedResponse } from '@validations/sdg.validation'
@@ -10,7 +11,7 @@ import { ActionResponseType } from '@validations/action.validation'
 
 import { FastifyZodInstance } from '@/types/fastify-zod'
 
-const tempUserId = 'aefeef22-5dfd-43d0-aac2-67135a07e482'
+const tempUserId = '682fc9a2-dfda-4ace-95ee-5bbaced67518'
 
 export default async (server: FastifyZodInstance) => {
   server.get(
@@ -75,6 +76,78 @@ export default async (server: FastifyZodInstance) => {
         events,
         actions: allActions,
         charities,
+      }
+    },
+  )
+
+  server.get(
+    '/suggested',
+    {
+      schema: {},
+    },
+    async (_request, _reply) => {
+      const userId = tempUserId
+
+      const actions = await Action.find({
+        $or: [{ participants: userId }, { followers: userId }],
+      }).populate('SDGs')
+
+      const sdgMap = new Map<string, number>()
+
+      actions.forEach(action => {
+        action.SDGs.forEach(sdg => {
+          // @ts-ignore
+          if (sdgMap.has(sdg.id)) {
+            // @ts-ignore
+            sdgMap.set(sdg.id, sdgMap.get(sdg.id) + 1)
+          } else {
+            // @ts-ignore
+            sdgMap.set(sdg.id, 1)
+          }
+        })
+      })
+
+      const sortedSdgs = Array.from(sdgMap.entries()).sort((a, b) => {
+        return b[1] - a[1]
+      })
+      const top5Sdgs = sortedSdgs.slice(0, 5)
+      const sdgIds = top5Sdgs.map(sdg => sdg[0])
+
+      const sdgs = await SDG.find({ _id: { $in: sdgIds } })
+      const otherSdgs = await SDG.find({ _id: { $nin: sdgIds } })
+
+      const allSdgs = [...sdgs, ...otherSdgs].slice(0, 5)
+      return allSdgs
+    },
+  )
+
+  server.get(
+    '/trending',
+    {
+      schema: {},
+    },
+    async (_request, _reply) => {
+      // get all actions that were created in the last 3 days and sort them by the number of participants
+      const returnedActions = await Action.find({})
+        .populate('creator')
+        .populate('participants')
+        .populate('followers')
+        .populate('withdrawals')
+        .populate('progress')
+
+      const sortedActions = returnedActions
+        .sort((a, b) => b.totalParticipantCount() - a.totalParticipantCount())
+        .slice(0, 5)
+
+      const returnedSDGs = await SDG.find().populate('followers')
+
+      const sortedSDGs = returnedSDGs
+        .sort((a, b) => b.followers.length - a.followers.length)
+        .slice(0, 5)
+
+      return {
+        actions: sortedActions,
+        sdgs: sortedSDGs,
       }
     },
   )
